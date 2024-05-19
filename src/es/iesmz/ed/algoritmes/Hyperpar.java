f8f5a585b62986f645dc77c407856a8e0ff0ceb7
Sumanlc <sumlam@alu.edu.gva.es>
2024-05-19 11:49:10 +0200

    Documentacion Hyperpar

--- src/es/iesmz/ed/algoritmes/Hyperpar.java
+++ src/es/iesmz/ed/algoritmes/Hyperpar.java
@@ -1,12 +1,24 @@
 package es.iesmz.ed.algoritmes;
 
+/**
+ * La classe Hyperpar determina si un nombre és hyperpar.
+ * Un nombre és hyperpar quan tots els seus dígits són parells.
+ */
 public class Hyperpar {
     private long numero;
 
+    /**
+     * Constructor de la classe Hyperpar.
+     * @param numero el número que es verificarà si és hyperpar.
+     */
     public Hyperpar(long numero) {
         this.numero = numero;
     }
 
+    /**
+     * Verifica si el número és hyperpar.
+     * @return true si el número és hyperpar, false en cas contrari.
+     */
     public boolean esHyperPar() {
         String numStr = Long.toString(numero);
         for (char c : numStr.toCharArray()) {
