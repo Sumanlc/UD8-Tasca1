8b53ca75094b107ee038f783d001ba1e643efee4
Sumanlc <sumlam@alu.edu.gva.es>
2024-05-19 14:21:18 +0200

    documentacion Any

--- src/es/iesmz/ed/algoritmes/Any.java
+++ src/es/iesmz/ed/algoritmes/Any.java
@@ -1,23 +1,24 @@
 package es.iesmz.ed.algoritmes;
 
-import java.util.HashSet;
-import java.util.Set;
-
+/**
+ * La classe Any determina el nombre de dígits diferents que té un any.
+ */
 public class Any {
     private long any;
 
+    /**
+     * Constructor de la classe Any.
+     * @param any l'any que es verificarà.
+     */
     public Any(long any) {
         this.any = any;
     }
 
+    /**
+     * Calcula el nombre de dígits diferents que té l'any.
+     * @return el nombre de dígits diferents.
+     */
     public int digitsDiferents() {
-        String anyStr = Long.toString(any);
-        Set<Character> digits = new HashSet<>();
-        for (char c : anyStr.toCharArray()) {
-            digits.add(c);
-        }
-        return digits.size();
+        return (int) Long.toString(any).chars().distinct().count();
     }
-
 }
-
